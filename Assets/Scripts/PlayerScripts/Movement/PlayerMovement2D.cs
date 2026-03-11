2b90da6732b17f8d48e3debd17202294fde480dc
Djumic Boris <Djumic.Boris@szl.intern>
2026-03-11 09:58:10 +0100

    MovementSprites added!

--- Assets/Scripts/PlayerScripts/Movement/PlayerMovement2D.cs
+++ Assets/Scripts/PlayerScripts/Movement/PlayerMovement2D.cs
@@ -4,12 +4,21 @@ public class PlayerMovement2D : MonoBehaviour
 {
     public float moveSpeed = 5f;
 
+    [Header("Sprites")]
+    public Sprite spriteIdle;
+    public Sprite spriteLeft;
+    public Sprite spriteRight;
+    public Sprite spriteUp;
+    public Sprite spriteDown;
+
     private Rigidbody2D rb;
     private Vector2 movement;
+    private SpriteRenderer sr;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -17,10 +26,29 @@ public class PlayerMovement2D : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         movement = movement.normalized;
+
+        UpdateSprite();
     }
 
     void FixedUpdate()
     {
         rb.linearVelocity = movement * moveSpeed;
+    }
+
+    void UpdateSprite()
+    {
+        if (movement == Vector2.zero)
+        {
+            sr.sprite = spriteIdle;
+        }
+        else if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
+        {
+            // Links/Rechts hat Priorität über Oben/Unten
+            sr.sprite = movement.x > 0 ? spriteRight : spriteLeft;
+        }
+        else
+        {
+            sr.sprite = movement.y > 0 ? spriteUp : spriteDown;
+        }
     }
 }
